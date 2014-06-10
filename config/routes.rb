95f68f4f1d276ef8ccb5be505b9bcf2b2b86b684
pulsen <huang123qwe@126.com>
2014-06-10 17:46:21 +0800

    测试

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :users
+
+
+  root "users#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -25,6 +30,8 @@ Rails.application.routes.draw do
   #       get 'sold'
   #     end
   #   end
+
+  
 
   # Example resource route with sub-resources:
   #   resources :products do
